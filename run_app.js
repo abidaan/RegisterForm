d82cd16a7299a78ab7a716de6262da40146f7ec4
shivajividhale <shivajividhale@gmail.com>
2015-11-18 17:42:36 -0500

    check featureflag

--- run_app.js
+++ run_app.js
@@ -31,7 +31,8 @@ run.app.post('/register',function(req, res){
         console.log(value);
         if(value=='true'){
         console.log("Email feature true ")
-        var sendgrid = require("sendgrid")(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
+        var sendgrid = require("sendgrid")(process.env.SENDGRID_USERNAME, "password");
+        // var sendgrid = require("sendgrid")(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
         var email = new sendgrid.Email();
         var sendemail = user.email;
         email.addTo(sendemail);
@@ -43,7 +44,7 @@ run.app.post('/register',function(req, res){
 
         sendgrid.send(email,function(err,json){
             if(err){
-                console.log(err)
+                //console.log(err)
                 client.set("emailFeature",false);
             }
         });

17abd52d84285c3ee641b920a5f316abd577a9b4
sganesh4 <sushil.89@gmail.com>
2015-11-17 19:17:01 -0500

    updating post build scripts

---
+++ scripts/deployScript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+image=""
+if [ $HOSTNAME == "prod" ]
+    image="sganesh4/regform:stable"
+else
+   image="sganesh4/regform:latest"
+fi
+docker pull $image
+docker stop webform
+docker rm webform
+docker rmi currnet
+docker tag $image current
+docker run -p 5001:3000 -d --add-host="redis_server:104.236.26.43" --name webform current
--- scripts/postBuildScript.sh
+++ scripts/postBuildScript.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
-docker build -t regform .
-docker tag regform sganesh4/regform:stable
+#start redis server if not running 
+redis_count=`ps -aef |grep redis-server|grep -v grep|wc -l`
+if [ $redis_count -eq 0 ]
+    echo "starting redis-server"
+    sudo redis-server &
+fi
+proxy_server_count=`ps -aef |grep redis-server|grep -v grep|wc -l`
+if [ $proxy_server_count -eq 0 ]
+    echo "starting proxy server"
+#    node proxy.js &
+fi
+sudo docker build -t regform .
+build_status=$?
+if [ $build_status -ne 0 ]
+  echo "docker build failed"
+  exit 1
+fi
+sudo docker tag regform sganesh4/regform:latest
+sudo docker push sganesh4/regform:stable
+if [ $? -ne 0 ]
+   echo "docker image push failed"
+   exit 2
+fi
+ssh root@prod << 'scripts/deployScript.sh'

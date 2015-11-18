b904fa796666298b5ced26eb0cf22f03e1750565
Abidaan <abidaan18@gmail.com>
2015-11-18 15:35:44 -0500

    Updated monitoring.js

--- Monitor/monitoring.js
+++ Monitor/monitoring.js
@@ -18,53 +18,51 @@ var headers = {
 //Request end point URL.
 var request = "https://api.newrelic.com/v2/applications/"+appID+"/metrics/data.json"
 
-
-//Figure this out if possible.
-setTimeout(function(){
-	//Setting the monitoring period to the last five minutes.
-	var startdate = new Date();
-	var currentdate = new Date();
-	startdate.setMinutes(currentdate.getMinutes() - 5);
-	var startDateTime = "\""+startdate.getFullYear()+"-"+(startdate.getMonth()+1)+"-"+startdate.getDate()+"T"
-					+startdate.getHours()+":"+startdate.getMinutes()+":"+startdate.getSeconds()+"+00:00\"";
-	var endDateTime = "\""+currentdate.getFullYear()+"-"+(currentdate.getMonth()+1)+"-"+currentdate.getDate()+"T"
-					+currentdate.getHours()+":"+currentdate.getMinutes()+":"+currentdate.getSeconds()+"+00:00\"";
+//The monitoring period which is currently set to the last five minutes.
+var startdate = new Date();
+var currentdate = new Date();
+startdate.setMinutes(currentdate.getMinutes() - 5);
+var startDateTime = "\""+startdate.getFullYear()+"-"+(startdate.getMonth()+1)+"-"+startdate.getDate()+"T"
+				+startdate.getHours()+":"+startdate.getMinutes()+":"+startdate.getSeconds()+"+00:00\"";
+var endDateTime = "\""+currentdate.getFullYear()+"-"+(currentdate.getMonth()+1)+"-"+currentdate.getDate()+"T"
+				+currentdate.getHours()+":"+currentdate.getMinutes()+":"+currentdate.getSeconds()+"+00:00\"";
 
 
-	//The data to be sent to New Relic to retrieve the average response time (avgRT) of the application(s) that is(are) being monitored
-	var avgResponseTimeData = {
-		'names':'HttpDispatcher',
-		"values":'average_call_time',
-		"from":'2015-11-17T00:00:00+00:00',
-		"to":endDateTime,
-		"summarize":'true'
-	}
+//The data to be sent to New Relic to retrieve the average response time of the application(s) that is(are) being monitored
+var avgResponseTimeData = {
+	'names':'HttpDispatcher',
+	"values":'average_call_time',
+	"from":'2015-11-17T00:00:00+00:00',
+	"to":endDateTime,
+	"summarize":'true'
+}
 
-	//API call for average response time
-	needle.post(request,avgResponseTimeData,{headers:headers},function(req, res){
-		//You can set the redis key-value pair here or in the second metric (see below)
-		var avg_response_time = JSON.stringify(res.body.metric_data.metrics[0].timeslices[0].values.average_call_time)
-		console.log(avg_response_time)
-	})
+//API call for average response time
+needle.post(request,avgResponseTimeData,{headers:headers},function(req, res){
+	//You can set the redis key-value pair here or in the second metric (see below)
+	var avg_response_time = JSON.stringify(res.body.metric_data.metrics[0].timeslices[0].values.average_call_time)
+	console.log(avg_response_time)
+})
 
-	//The data to be sent to New Relic to retrieve the application throughput values of the application(s) that is(are) being monitored
-	var errorRateData = {
-		'names':'HttpDispatcher',
-		"values":'call_count',
-		"from":"2015-11-17T00:00:00+00:00",
-		"to":endDateTime,
-		"summarize":'true'
-	}
+//The data to be sent to New Relic to retrieve the application throughput values of the application(s) that is(are) being monitored
+var errorRateData = {
+	'names':'HttpDispatcher',
+	"values":'call_count',
+	"from":"2015-11-17T00:00:00+00:00",
+	"to":endDateTime,
+	"summarize":'true'
+}
 
-	//API call for error count
-	needle.post(request,errorRateData,{headers:headers},function(req, res){
-		//You can set the redis key-value pair here.
-		var error_count = JSON.stringify(res.body.metric_data.metrics[0].timeslices[0].values.call_count);
-		console.log(error_count)
-		if(error_count != 0)
-			client.set("canaryDead",true);
-	})
-}, 10000);
+//API call for error count
+needle.post(request,errorRateData,{headers:headers},function(req, res){
+
+//You can set the redis key-value pair here.
+var error_count = JSON.stringify(res.body.metric_data.metrics[0].timeslices[0].values.call_count);
+console.log(error_count)
+if(error_count != 0)
+	client.set("canaryDead",true);
+})
+
 
 
 

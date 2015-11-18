e4a552b94f0cfb8956a2f7ebdf4fb4f47ec93f79
sganesh4 <sushil.89@gmail.com>
2015-11-18 01:07:11 -0500

    Configuring newrelic

--- run_app.js
+++ run_app.js
@@ -1,6 +1,7 @@
 var run = require('./app.js');
 var redis = require('redis')
 var client = redis.createClient(6379, 'redis_server', {})
+require('newrelic')
 run.app.post('/register',function(req, res){
     var user = {
         firstName: req.body.firstname,
